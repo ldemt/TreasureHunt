a00f581bd3249a89a14afc61eeeeba965bfc6829
Leo D <leodemont@gmail.com>
2023-12-11 17:01:40 +0100

    added a console application for easier debugging in VS

---
+++ TestAppTHunt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TreasureHunt;
+namespace TestAppTHunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CoolPlayer player = new CoolPlayer();
+
+            string checkCoolness = player.coolness;
+        }
+    }
+}
