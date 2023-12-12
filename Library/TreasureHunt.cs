4a5aac8c87e358f82f86cac8bf5ac18bc46d9e8e
selwy <selwy@LAPTOP-I9OHPP3M>
2023-12-12 10:38:44 +0100

    SelwynPlayer

--- Library/TreasureHunt.cs
+++ Library/TreasureHunt.cs
@@ -159,11 +159,11 @@ namespace TreasureHunt
 
     }
 
-    public class CustomPlayer : Player
+    public class SelwynPlayer : Player
     {
         public string coolness;
 
-        public CustomPlayer(double x, double y) : base(x,y)
+        public SelwynPlayer(double x, double y) : base(x,y)
         {
             coolness = "very cool";
         }
