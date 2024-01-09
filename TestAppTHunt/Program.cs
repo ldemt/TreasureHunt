66034e3dfe0ffab72ade5d24cef22bb08ebe5782
Leo D <leodemont@gmail.com>
2024-01-09 11:48:20 +0100

    Example of benchmark program

--- TestAppTHunt/Program.cs
+++ TestAppTHunt/Program.cs
@@ -2,6 +2,10 @@
 using System.Threading;
 using TreasureHunt;
 using Rhino.Geometry;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
 namespace TestAppTHunt
 {
     internal class Program
@@ -11,19 +15,39 @@ namespace TestAppTHunt
             Console.WriteLine("## TreasureHunt v0.0 Demo Program | DBD OOP Course 2023-2024 ## ");
             Console.WriteLine("Initializing...");
 
-            InitializeGame();
-            RunGame();
+
+            // Base code to run game in CLI and see variables.
+            //InitializeGame();
+            //RunGame();
+
+            // Code to run game in benchmark mode
+            Benchmarker benchmarker = new Benchmarker();
+
+            // Write benchmark results to .csv file
+            var csv = new StringBuilder();
+
+            for (int i = 0; i < 2500; i++) {
+                List<int> numberOfSteps = benchmarker.RunBenchmark();
+                Console.Write(String.Format("Benchmark {0:000} Finished. Steps : {1} \n", i, numberOfSteps[0]));
+
+                var newLine = string.Format("{0}", numberOfSteps[0]);
+                csv.AppendLine(newLine);
+            }
+
+            string filePath = "C:/Users/Leo/source/repos/TreasureHunt/BenchmarkResults/testBenchmark.csv";
+            File.WriteAllText(filePath, csv.ToString());
+
 
         }
 
         static void InitializeGame()
         {
-            
-            
+
+
             double xSize = 600;
             double ySize = 300;
             double numberOfAgents = 20;
-       
+
 
             //NEW Instantiate wormholes and add em to the wormholeList of StaticGame
             int numberOfWormholes = 3;
@@ -37,7 +61,7 @@ namespace TestAppTHunt
 
                 double wormholeX = randomWormhole.NextDouble() * xSize;
                 double wormholeY = randomWormhole.NextDouble() * ySize;
-             
+
 
 
 
@@ -49,7 +73,7 @@ namespace TestAppTHunt
             }
 
 
-            
+
             // Generate and initialize the players
             Random random = new Random(7897999);
             // ZigzagPlayer player = new TreasureHunt.ZigzagPlayer();
@@ -60,14 +84,14 @@ namespace TestAppTHunt
             {
                 randomX = random.NextDouble() * xSize;
                 randomY = random.NextDouble() * ySize;
-                
+
                 Player player = new Player(randomX, randomY);
                 // Instantiate Hugo agents (one follower for each player)
                 HugoFollower follower = new HugoFollower(player);
                 // Add them to the list of players belonging to the space.
-                
-               
-                
+
+
+
                 // Add it to the list of balls belonging to the space.
                 StaticGame.playerList.Add(player);
                 StaticGame.playerList.Add(follower);
@@ -82,13 +106,13 @@ namespace TestAppTHunt
             ZigzagPlayer myCustomPlayer = new ZigzagPlayer(x, y, 0, 0, 600, 300);
             SpiralMovement myCustomPlayer2 = new SpiralMovement(20, 50);
             //CustomPlayer myCustomPlayer = new CustomPlayer(10,10);
-            AlonsoPlayer alonsoPlayer = new AlonsoPlayer(new Vector3d(10,10,0), new Vector3d(1,1,0));
+            AlonsoPlayer alonsoPlayer = new AlonsoPlayer(new Vector3d(10, 10, 0), new Vector3d(1, 1, 0));
             // Add here your custom player to the player list!
             StaticGame.playerList.Add(myCustomPlayer);
             StaticGame.playerList.Add(myCustomPlayer2);
 
             // Adding Tin player classes
-            TinLeader tinLeader = new TinLeader(10,10);
+            TinLeader tinLeader = new TinLeader(10, 10);
             TinFollower tinFollower = new TinFollower(10, 10, xSize, ySize);
             Team team = new Team(tinLeader, tinFollower);
 
@@ -121,7 +145,7 @@ namespace TestAppTHunt
 
                 }
 
-                    for (int i = 0; i < StaticGame.playerList.Count; i++)
+                for (int i = 0; i < StaticGame.playerList.Count; i++)
                 {
                     Console.Write(String.Format("PLAYER {0:00} | {1} | Position (X,Y) : {2:0.00}, {3:0.00} \n", i, StaticGame.playerList[i].statusMessage, StaticGame.playerList[i].location.X, StaticGame.playerList[i].location.Y));
 
@@ -138,4 +162,82 @@ namespace TestAppTHunt
 
         static Game StaticGame { get; set; }
     }
+
+    public class Benchmarker
+    {
+        Game game;
+        Random random;
+        double xSize;
+        double ySize;
+        int numberOfAgents;
+        int numberOfWormholes;
+        int numberOfObstacles;
+
+        public Benchmarker()
+        {
+            numberOfAgents = 1;
+            // Init game
+            xSize = 600;
+            ySize = 300;
+
+            game = new Game(xSize, ySize);
+
+            random = new Random();
+
+            InitializeGame();
+        }
+
+        void InitializeGame()
+        {
+
+            double randomX;
+            double randomY;
+
+            game.playerList.Clear();
+
+            randomX = RandomProvider.Instance.NextDouble() * xSize;
+            randomY = RandomProvider.Instance.NextDouble() * ySize;
+            
+            game.treasure = new Treasure(randomX, randomY);
+            game.isTreasureFound = false;
+
+            for (int i = 0; i < numberOfAgents; i++)
+            {
+                randomX = RandomProvider.Instance.NextDouble() * xSize;
+                randomY = RandomProvider.Instance.NextDouble() * ySize;
+
+                Player player = new Player(randomX, randomY);
+                // Add them to the list of players belonging to the space.
+                // Add it to the list of balls belonging to the space.
+                game.playerList.Add(player);
+
+            }
+
+        }
+        public List<int> RunBenchmark()
+        {
+            List<int> stepsCount = new List<int>();
+
+            while (game.isTreasureFound == false)
+            {
+                game.UpdatePlayers();
+
+            }
+
+            
+
+            foreach (Player player in game.playerList)
+            {
+                stepsCount.Add(player.stepsNumber);
+            }
+            
+            // Reset the game
+            InitializeGame();
+            return stepsCount;
+        
+        }
+
+    }
+
+
 }
